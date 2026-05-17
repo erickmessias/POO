4e92389e48c1b9f0e47bdba300867b6d1a8dca8f
erickmessias <erickmessias195@gmail.com>
2026-05-17 09:54:41 -0300

    Add files via upload

--- EXERCICIOS/app.java
+++ EXERCICIOS/app.java
@@ -1,9 +1,25 @@
 
-
+import java.util.Scanner;
 public class app {
+
     public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
         Pedido cardapio = new Pedido();
-        Pedido pedido1 = new Pedido("salgado",7,0);
+        System.out.println("informe seu pedido");
+
+        String cliete = ler.nextLine();
+        cardapio.setItem(cliete);
+
+
+
+        System.out.println("informe a quantidade");
+        int quant = ler.nextInt();
+        cardapio.setQuantidade(quant);
+        cardapio.setPrecoUnitario(cardapio.buscarPreco(cliete));
+
+        System.out.println("Você pediu " +cardapio.getQuantidade() +" " +cardapio.getItem());
+        System.out.println("Seu pedido esta no valor de: "+cardapio.calcularTotal());
+
 
         }
     }
